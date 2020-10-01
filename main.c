a4a5b7e9b6b9230e4f7ad5f33fd33bee001c9e33
Priyansh-jsk <58244081+Priyansh-jsk@users.noreply.github.com>
2020-10-01 09:28:27 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-//I've made this project in the first few weeks in the "Structured Programming in C" course so the code isnt so clean XD
+//I and my friend made this project in first few weeks in the "Structured Programming in C" course so the code isnt so clean XD
 //Enjoy the game by pressing "Run" 
 #include <stdio.h>
 #include <time.h>
